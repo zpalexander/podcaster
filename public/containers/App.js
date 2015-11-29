340369780d6ac71d690ec08821ad624628e6b397
Zach Alexander <zpalexander@gmail.com>
2015-11-28 22:20:58 -0500

    Add sass to project

--- public/containers/App.js
+++ public/containers/App.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import '!style!css!sass!../styles/styles.scss';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { fetchFeeds } from '../actions/feeds';
--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,10 @@ module.exports = {
       loaders: [ 'babel' ],
       exclude: /node_modules/,
       include: __dirname
+    },
+    {
+      test: /\.scss$/,
+      loader: 'style!css!sass?sourceMap'
     }]
   },
 }
