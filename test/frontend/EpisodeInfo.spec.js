24a4ac928fdebe7ef6845a46571924c59e32cdaa
Zach Alexander <zpalexander@gmail.com>
2016-01-18 16:20:15 -0500

    Add travis config

--- test/frontend/EpisodeInfo.spec.js
+++ test/frontend/EpisodeInfo.spec.js
@@ -6,6 +6,7 @@
 /* Dependencies */
 // Libraries and testing utilities
 import React from 'react';
+import moment from 'moment';
 import expect from 'expect';
 import expectJSX from 'expect-jsx';
 import {createRenderer} from 'react-addons-test-utils';
@@ -13,17 +14,21 @@ expect.extend(expectJSX);
 // Component
 import EpisodeInfo from '../../public/components/EpisodeInfo';
 
+
 /* Mock Props */
 const activeEpisodeHandler = () => {};
 
+
 /* Helpers */
-const buildComponent = () => {
+const buildComponent = (unplayed, date) => {
     let episode = {
         _id: 'abcdef123456',
-        name: '',
-        feedName: '',
-        pubDate: new Date()
+        name: 'Test Episode',
+        feedName: 'Test Feed',
+        unplayed: unplayed,
+        pubDate: date
     };
+
     let renderer = createRenderer();
     renderer.render(
         <EpisodeInfo
@@ -34,13 +39,25 @@ const buildComponent = () => {
     return renderer.getRenderOutput();
 };
 
+
 /* Expected Constants */
 const expectedClassname = 'episode-info';
 
+
 /* Test */
 describe('Components: EpisodeInfo', () => {
+
     it('has a classname "' + expectedClassname + '" that matches the component name', () => {
-        let actualElement = buildComponent();
+        let actualElement = buildComponent(false, new Date());
         expect(actualElement.props.className).toBe(expectedClassname);
     });
+
+    it('displays the date in the right format', () => {
+        let myDate = new Date();
+        let expectedResult = moment(myDate).format('ddd, MMMM Do')
+        let actualElement = buildComponent(false, new Date());
+        let dateWrapper = actualElement.props.children[2];
+        expect(dateWrapper.props.children).toBe(expectedResult);
+    });
+
 });
