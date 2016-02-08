1fb5cace4ef4bdc95ceb9ce29ae4f99cd3ff83ac
Zach Alexander <zpalexander@gmail.com>
2016-02-07 20:24:20 -0500

    Set up routing

--- public/index.js
+++ public/index.js
@@ -10,18 +10,27 @@ import 'babel-core/polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-// import { Router, Route, IndexRoute } from 'react-router';
-// import { createHistory } from 'history';
-// import { syncReduxAndRouter } from 'redux-simple-router';
+import { Router, Route, IndexRoute } from 'react-router';
+import { createHistory } from 'history';
+import { syncReduxAndRouter } from 'redux-simple-router';
 /* Containers */
 import App from './containers/App';
+import UI from './containers/UI';
+/* Store */
 import configureStore from './store/configureStore';
 
 const store = configureStore();
+const history = createHistory();
+
+syncReduxAndRouter(history, store);
 
 render(
     <Provider store={store}>
-        <App />
+        <Router history={history}>
+            <Route path='/' component={App}>
+                <IndexRoute component={UI} />
+            </Route>
+        </Router>
     </Provider>,
     document.getElementById('root')
 );
--- public/reducers/index.js
+++ public/reducers/index.js
@@ -3,8 +3,8 @@
  *
  * Entry point for all reducers
  */
-
 import { combineReducers } from 'redux';
+import { routeReducer } from 'redux-simple-router';
 import feeds from './feeds';
 import episodes from './episodes';
 import activeFeed from './activeFeed';
@@ -14,7 +14,8 @@ const rootReducer = combineReducers({
     feeds,
     episodes,
     activeFeed,
-    activeEpisode
+    activeEpisode,
+    routing: routeReducer
 });
 
 export default rootReducer;
