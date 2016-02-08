4ef7ba8009a29c64967c7369e590061a651db6fd
Zach Alexander <zpalexander@gmail.com>
2016-02-08 13:38:11 -0500

    Add refresh icon to header

--- public/components/UI/Header.js
+++ public/components/UI/Header.js
@@ -6,13 +6,28 @@
 
 /* Dependencies */
 import React, { Component } from 'react'
+import Refresh from 'react-icons/lib/md/refresh';
 
 /* Component Definition */
 class Header extends Component {
+
+    renderRefreshIcon() {
+        let refreshIcon = React.createElement(Refresh, null);
+        return (
+            <div className='refresh-icon'>
+                {refreshIcon}
+            </div>
+        )
+    }
+
     render() {
+        let refreshIcon = this.renderRefreshIcon();
+
+
         return (
           <header className="header">
               <h1>Podcasts</h1>
+              {refreshIcon}
           </header>
         )
     }
