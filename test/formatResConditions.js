6ccc64cb3065b8aa2b892e7376a460529be74b0d
Zach Alexander <zpalexander@gmail.com>
2016-03-05 22:13:39 -0500

    Reformat header css

---
+++ test/formatResConditions.js
@@ -0,0 +1,40 @@
+(function() {
+    'use strict';
+    /**
+     * formatResConditions.js
+     *
+     * Helper for writing Express
+     * controller unit tests
+     */
+     var _ = require('lodash');
+
+    var formatResConditions = function(resultFuncs, done) {
+        var modifiedResultFuncs = {};
+        var names = Object.getOwnPropertyNames(resultFuncs);
+        var funcsProcessed = [];
+
+        _.forOwn(resultFuncs, function(resultFunc, resultFuncName) {
+            funcsProcessed.push(resultFuncName);
+            var modifiedFunc;
+
+            if (names.length === funcsProcessed.length) {
+                modifiedFunc = function(arg) {
+                    resultFunc(arg);
+                    done();
+                };
+            } else {
+                modifiedFunc = function(arg) {
+                        resultFunc(arg);
+                        return this;
+                };
+            }
+
+            modifiedResultFuncs[resultFuncName] = modifiedFunc;
+        });
+
+        return modifiedResultFuncs;
+    };
+
+    exports.default = formatResConditions;
+
+}());
