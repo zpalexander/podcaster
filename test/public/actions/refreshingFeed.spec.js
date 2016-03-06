848ebeff037ecc36e15bb67196aeb7ca40c3fb8c
Zach Alexander <zpalexander@gmail.com>
2016-03-06 14:20:38 -0500

    Unit tests: refreshingFeed actions

---
+++ test/public/actions/refreshingFeed.spec.js
@@ -0,0 +1,45 @@
+/**
+ * refreshingFeed.spec.js
+ *
+ * Unit tests for the refreshingFeed actions
+ */
+
+/* Dependencies */
+// Libraries and testing tools
+import expect from 'expect';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import nock from 'nock';
+// Files to be tested
+import * as actions from '../../../public/actions/refreshingFeed';
+import * as types from '../../../public/constants/ActionTypes';
+
+/* Mocks */
+//const middlewares = [ thunk ];
+//const mockStore = configureMockStore(middlewares);
+
+
+/* Tests */
+describe('Frontend - Actions: RefreshingFeed', () => {
+    afterEach(() => {
+        nock.cleanAll()
+    });
+
+
+    it('should create an action to signal a feed refresh request', () => {
+        const expectedAction = {
+            type: types.REQUEST_REFRESH
+        };
+        expect(actions.requestRefresh()).toEqual(expectedAction);
+    });
+
+
+    it('should create an action to signal the completion of a feed refresh request', () => {
+        const expectedAction = {
+            type: types.COMPLETE_REFRESH,
+            result: {}
+        };
+        expect(actions.completeRefresh({})).toEqual(expectedAction);
+    });
+
+});
